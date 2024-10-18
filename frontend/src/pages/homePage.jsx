fdbcdeb0e21050725a5ad052956ed8f517f961fc
Biplab <jbiplabmahato@gmail.com>
2024-10-18 23:02:57 +0530

    service page AND ABOUT

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import Navbar from '../component/Navbarr'
-function homePage() {
+import React from 'react';
+import { useNavigate } from 'react-router-dom';    
+import Navbar from '../component/Navbarr';                
+import { Button } from "flowbite-react";                  
+
+const HomePage = () => {
+  // const navigate = useNavigate();  
+
+  // const handleClick = () => {
+  //   navigate('./page1');  
+  // };
+
   return (
     <>
-        <Navbar/>
+      <Navbar />  {/* Your Navbar component */}
+      
+      {/* <div style={{ margin: '20px' }}>
+        <Button color="blue" pill onClick={handleClick}>  
+          Go to Page 1
+        </Button>
+      </div> */}
     </>
-  )
-}
+  );
+};
 
-export default homePage
+export default HomePage;
